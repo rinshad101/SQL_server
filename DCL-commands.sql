cab04206719b11e648aa84e635d4a87eb7185691
rinshad101 <rinshadkk1111@gmail.com>
2025-03-11 11:04:23 +0530

    new

---
+++ DCL-commands.sql
@@ -0,0 +1,6 @@
+-- GRANT – Gives specific permissions to a user
+GRANT SELECT, INSERT ON myTable TO user1;
+
+-- REVOKE – Removes specific permissions from a user
+REVOKE INSERT ON myTable FROM user1;
+
