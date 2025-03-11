ee16b5fcff10f7a5d043c986d56571b351bb31cd
rinshad101 <rinshadkk1111@gmail.com>
2025-03-11 10:00:33 +0530

    new

--- DDL-commands.sql
+++ DDL-commands.sql
@@ -1,3 +1,21 @@
 -- Create a New Database
-CREATE DATABASE muDB;
+CREATE DATABASE myDB;
 
+-- Use the Created Database
+USE myDB;
+
+-- Create a Table
+CREATE TABLE myTable (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+    );
+
+-- Add a new column to table
+ALTER TABLE myTable ADD phNo VARCHAR(10);
+
+-- Delete all Records in a table but keep the structure
+TRUNCATE TABLE myTable
+
+-- Permananently delete a table
+DROP TABLE myTable;
